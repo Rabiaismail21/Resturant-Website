40e58d30b3d43a21c19ecf818ee7e31716728be2
Rabiaismail21 <rabiaismailsakhi@gmail.com>
2024-03-19 03:02:33 +0500

    Fixed Gallery Issue

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
+// smooth scroll (scroll reveal js library)
 ScrollReveal({
   reset: false,
   distance: "60px",
@@ -11,23 +12,25 @@ ScrollReveal().reveal("h2", { delay: 50, origin: "left" });
 
 // food gallery height
 
-const gallery = document.getElementById("gallery");
+const gallery = document.getElementById("gallery-container");
 const moreBtn = document.getElementById("more-btn");
 
 function toggleHeight(){
     if (moreBtn.textContent == "Less") {
-        gallery.classList.replace("h-full", "h-70vh");
+        gallery.classList.replace("md:h-full", "md:h-70vh");
         moreBtn.textContent = "More";
       } else {
-        gallery.classList.replace("h-70vh", "h-full");
+        gallery.classList.replace("md:h-70vh", "md:h-full");
         moreBtn.textContent = "Less";
       }
 }
 moreBtn.addEventListener("click", toggleHeight);
 
+// update copyright year in footer dynamically
 let copyright_Year = document.querySelector(".copyright-year");
 copyright_Year.textContent = new Date().getFullYear();
 
+// navbar collapse in mobile view
 let toggle_menu_btn = document.querySelector("#toggle-menu-btn");
 let nav_menu = document.querySelector("#nav-items");
 
@@ -40,11 +43,5 @@ const toggleMenu = () => {
     toggle_menu_btn.innerHTML = '<i class="fa-solid fa-bars"></i>';
   }
 };
-
-// document.querySelector('.fa-xmark').addEventListener('click', ()=>{
-//     nav_menu.classList.add('h-0', 'overflow-hidden');
-//     toggle_icons.classList.replace('fa-xmark', 'fa-bars');
-// })
-
 toggle_menu_btn.addEventListener("click", toggleMenu);
 
